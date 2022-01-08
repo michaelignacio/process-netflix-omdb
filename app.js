c10320b524653ac712c2e33071b88995261b8d55
Michael <mikelignacio@gmail.com>
2022-01-09 00:32:37 +0800

    Final version

--- app.js
+++ app.js
@@ -3,28 +3,41 @@ const OMDB_API_KEY = '85c342da';
 const nReadlines = require('n-readlines');
 const axios = require('axios');
 const fs = require('fs');
+const slugify = require('slugify');
 
 const broadbandLines = new nReadlines('ratings.csv');
 
 let line;
 let lineNumber = 1;
+let moviesData = [];
 
 while ((line = broadbandLines.next())) {
   // console.log(`Line ${lineNumber} has: ${line.toString('ascii')}`);
   let arr = line.toString().split(',');
   let movieTitle = arr[1];
-  let moviesData = [];
 
   console.log('Searching for ' + movieTitle);
 
   axios
     .get(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${movieTitle}`)
     .then(function (response) {
-      console.log(response.data);
-      moviesData.push(response.data);
-      fs.appendFile(
+      // console.log(response.data);
+      let currentMovie = response.data;
+      if (arr[4] == 1) {
+        currentMovie['jalThumbs'] = 'down';
+      } else if (arr[4] == 2) {
+        currentMovie['jalThumbs'] = 'up';
+      }
+      currentMovie['jalRatedAt'] = arr[6];
+      currentMovie['slug'] = slugify(arr[1].toLowerCase(), {
+        remove: /[*+~.()'"!:@]/g,
+      });
+      // moviesData.push(response.data);
+      moviesData.push(currentMovie);
+      fs.writeFile(
         'movies.json',
         JSON.stringify(moviesData, null, 2),
+        // moviesData,
         function (err) {
           if (err) {
             return console.log(err);
@@ -37,8 +50,6 @@ while ((line = broadbandLines.next())) {
     })
     .then(function () {});
   lineNumber++;
-
-  if (lineNumber == 2) break;
 }
 
 // console.log('end of file.');
